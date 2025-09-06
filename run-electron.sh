60c098f96a84ae8ea74137ba43827f1f5be31621
Luis Majano <lmajano@gmail.com>
2025-09-06 23:37:15 +0700

    wip

--- run-electron.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set app name environment variable
-export ELECTRON_APP_NAME="BoxLang Starter Desktop"
-
-# Run electron with development environment
-NODE_ENV=development npx electron .electron/main.js
